5035f0f12965542978906c3344ba9651e50ffb3c
ophlip19-ship-it <ophlip19@gmail.com>
2026-02-15 18:30:26 +0100

    Add files via upload

---
+++ harm.js
@@ -0,0 +1,13 @@
+
+// const harmMenu = document.querySelector('.harmburger');
+// const offscreenMenu = document.querySelector('.off-screenMenu');
+// harmMenu.addEventListener ('click', () =>{
+    // harmMenu.classList.toggle('active');
+    // offscreenMenu.classList.toggle('active');
+// })
+const harmBurg = document.querySelector('.harmburger');
+const offScreen = document.querySelector('.off-screen');
+harmBurg.addEventListener ('click', () => {
+   harmBurg.classList.toggle('active');
+   offScreen.classList.toggle('active');
+})
